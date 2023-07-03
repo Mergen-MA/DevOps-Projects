34a59231a0c4b14e246ed1617d4c014511ece523
Mergen-MA <malidinc13@gmail.com>
2023-07-03 16:53:14 +0200

    control_node was launched

--- Project-207_Ansible-publish-website-postgresql-nodejs-react/terraform-env-setup/main.tf
+++ Project-207_Ansible-publish-website-postgresql-nodejs-react/terraform-env-setup/main.tf
@@ -15,4 +15,12 @@ provider "aws" {
 
 resource "aws_instance" "nodes" {
     ami = var.myami
+    instance_type = var.controlinstancetype
+    key_name = var.mykey
+    iam_instance_profile = aws_iam_instance_profile.ec2full.name 
+    vpc_security_group_ids = [aws_security_group.tf-sec-gr.id]
+    tags = {
+        Name = "ansible-control"
+        stack = "ansible-project"
+    }
 }
