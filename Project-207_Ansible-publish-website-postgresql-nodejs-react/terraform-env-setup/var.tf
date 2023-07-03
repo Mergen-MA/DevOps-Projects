74460a7653a75961348285a7f69a957a56bd13da
Mergen-MA <malidinc13@gmail.com>
2023-07-03 16:07:05 +0200

    terraform variables are set

---
+++ Project-207_Ansible-publish-website-postgresql-nodejs-react/terraform-env-setup/var.tf
@@ -0,0 +1,11 @@
+variable "region" {}
+variable "mykey" {}
+variable "tags" {}
+variable "instancetype" {}
+variable "controlinstancetype" {}
+variable "num" {}
+variable "mysecgrp" {}
+variable "user" {}
+variable "myami" {
+    description = "redhat enterprise linux 8 ami"
+}
